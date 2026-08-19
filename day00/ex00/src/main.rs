9ed81783b0a801c28dc31a8caf90a188ad1d9b95
mdfpva <mdfpva@protonmail.com>
2026-08-19 17:20:08 +0100

    ex00/

---
+++ day00/ex00/src/main.rs
@@ -0,0 +1,16 @@
+/*
+ex00 — Hello, Piscine!
+Programa que imprime exatamente:
+
+Hello, Piscine!
+Antes de passares ao seguinte, corre os três comandos que vais correr o resto da piscine:
+
+bash
+cargo run
+cargo fmt --check
+cargo clippy -- -D warnings
+*/
+
+fn main() {
+    println!("Hello, Piscine!");
+}
